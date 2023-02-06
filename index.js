2d4697db818e03eac557ff1375238002fd3d9387
josefleming <Jose.Augustine@flemingcollege.ca>
2023-02-05 20:39:07 -0500

    minor changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require(["esri/config","esri/Map","esri/views/MapView"
 ], function(esriConfig,Map, MapView) {
-    esriConfig.apiKey = "AAPK12c6320197704f18ae014300c0c8a640LRp_8AJyCI48y66viGFlfywMHBuZRiCvCvaLXBgPd71ZQ5dB7dOwtOoCdOclz67T";
+    esriConfig.apiKey = "Your API key";
   const map = new Map({
     basemap: "arcgis-imagery" // Basemap layer service
     });
